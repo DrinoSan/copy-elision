ec45651e2bd4da34108844fad28f82c917a05bd5
DrinoSan <s.becirbegovic@gmail.com>
2024-09-28 09:17:11 +0200

    clang format

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 //  -fno-elide-constructors
 // use -wall for unnecessary calls to move
 
-
 #include <iostream>
 
 class foo
@@ -10,9 +9,12 @@ class foo
   public:
     foo() { std::cout << "called constructor\n"; }
     foo( const foo& other ) { std::cout << "called copy constructor\n"; }
-	 foo& operator=(const foo& rhs ) { std::cout << "called assignemnt constructor\n"; return *this; }
+    foo& operator=( const foo& rhs )
+    {
+        std::cout << "called assignemnt constructor\n";
+        return *this;
+    }
     foo( foo&& other ) { std::cout << "Called move constructor\n"; }
-
 
     ~foo() { std::cout << "destructor called\n"; }
 
@@ -22,7 +24,7 @@ class foo
 foo f()
 {
     std::cout << "-- inside f() before constructing foo\n";
-    foo blub; // 2) Calling Constructor
+    foo blub;   // 2) Calling Constructor
     std::cout << "-- inside f() after constructing foo\n";
     return blub;
 }
@@ -30,16 +32,16 @@ foo f()
 foo f_nrvo_multiple_paths( int32_t x )
 {
     std::cout << "-- inside f() before constructing foo\n";
-    if( x % 2 == 0 )
+    if ( x % 2 == 0 )
     {
-       std::cout << "Foo1\n";
-       foo foo1;
-       return foo1;
+        std::cout << "Foo1\n";
+        foo foo1;
+        return foo1;
     }
 
-	std::cout << "Foo2\n";
-	foo foo2;
-	return foo2;
+    std::cout << "Foo2\n";
+    foo foo2;
+    return foo2;
 }
 
 foo f_rvo()
@@ -48,18 +50,18 @@ foo f_rvo()
     return foo();
 }
 
-void f_nrvo_multiple_paths_working( foo& foo12, bool flag)
+void f_nrvo_multiple_paths_working( foo& foo12, bool flag )
 {
     std::cout << "-- inside f() before constructing foo\n";
 
-    if(flag)
+    if ( flag )
     {
-		foo12.i = 123;
-	   return;
+        foo12.i = 123;
+        return;
     }
 
-	 foo12.i = 1222;
-	 return;
+    foo12.i = 1222;
+    return;
 }
 
 int main()
@@ -69,10 +71,3 @@ int main()
 
     return 0;
 }
-
-
-
-
-
-
-
